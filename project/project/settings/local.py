46c961f34751e1182e451c9990a8b6a8632a0d1d
smileoops <dinisa2010@gmail.com>
2022-05-11 14:53:00 +0300

    django_skeleton

---
+++ project/project/settings/local.py
@@ -0,0 +1,24 @@
+from .settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-m08c-&1uyts$_nco(swyjrc19l9%u2!5$-=8__6wt12@w%#o)6'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
